a8e251b3b665be8f7fc0b1555a5d281f516b203b
Amritendu Mohan Roy <amritenduroy70@gmail.com>
2022-01-08 01:35:05 +0530

    initial commit

---
+++ app/src/main/java/com/example/newsfresh/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.example.newsfresh
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+
+//
+
+class MainActivity : AppCompatActivity() , NewsItemClicked{
+    private lateinit var mAdapter: NewsListAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView);
+        recyclerView.layoutManager = LinearLayoutManager(this);
+        fetchData();
+
+        mAdapter = NewsListAdapter( this);
+        recyclerView.adapter = mAdapter;
+
+
+    }
+    private fun fetchData() {
+        val url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=faebd857fe9d4c4695c7f20181de1ec3"
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            null,
+            {
+                val newsJsonArray = it.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for(i in 0 until newsJsonArray.length()) {
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage")
+                    )
+                    newsArray.add(news)
+
+                }
+
+                mAdapter.updateNews(newsArray)
+            },
+            {
+
+            }
+        )
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
+//    private fun fetchData()
+//    {
+//    val url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=faebd857fe9d4c4695c7f20181de1ec3"
+//        val jsonObjectRequest = JsonObjectRequest(
+//            Request.Method.GET, url, null,
+//            Response.Listener { response ->
+//            val newsJsonArray = response.getJSONArray("articles")
+//                val newsArray = ArrayList<News>();
+//                for(i in 0 until newsJsonArray.length())
+//                {
+//                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+//                    val news = News(
+//                        newsJsonObject.getString("title"),
+//                        newsJsonObject.getString("author"),
+//                        newsJsonObject.getString("url"),
+//                        newsJsonObject.getString("urlToImage")
+//                    )
+//                    newsArray.add(news);
+//                }
+//                mAdapter.updateNews(newsArray)
+//            },
+//            Response.ErrorListener { error ->
+//
+//            }
+//        )
+//
+//// Access the RequestQueue through your singleton class.
+//        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+//
+//    }
+
+    override fun onItemClicked(item: News) {
+        val builder =  CustomTabsIntent.Builder()
+        val customTabsIntent = builder.build()
+        customTabsIntent.launchUrl(this, Uri.parse(item.url))
+  }
+}
